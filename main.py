9a828056d7912807ce75740c6b715886d90466f2
ellis3684 <ellis3684@yahoo.com>
2022-03-04 21:09:24 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+
+# Morse dictionary for all Latin alphabet characters, numbers 0-9, and the most commonly seen punctuation.
+morse_dict = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "0": "-----",
+    "1": "",
+    "2": "",
+    "3": "",
+    "4": "",
+    "5": "",
+    "6": "",
+    "7": "",
+    "8": "",
+    "9": "",
+    " ": "/",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "'": ".----.",
+    "!": "-.-.--",
+    "/": "-..-.",
+    "(": "-.--.",
+    ")": "-.--.-",
+    "&": ".-...",
+    ":": "---...",
+    ";": "-.-.-.",
+    "=": "-...-",
+    "+": ".-.-.",
+    "-": "-....-",
+    "_": "..--.-",
+    "\"": ".-..-.",
+    "$": "...-..-",
+    "@": ".--.-.",
+}
+
+user_text = input("What would you like to say in Morse code? ")
+morse_output = ""
+
+# Loops through the user's string input, and returns the Morse equivalent. If the user's input includes any input
+# that is outside of the scope of the Morse dictionary I hardcoded, the loop ignores it and continues.
+for char in user_text:
+    if char in morse_dict:
+        morse_output += morse_dict[char]
+        morse_output += " "
+    else:
+        continue
+
+# The Morse output is printed in the output below.
+print(morse_output)
